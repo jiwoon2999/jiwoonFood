bd5e5c8eb1b8dc147d200b1db2ca1d047919a42d
jiwoon2999 <jiwoon2999@naver.com>
2023-02-10 00:12:15 +0900

    메인슬라이드 유사배열객체 생성

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@
 
 const menuList = document.querySelectorAll('.menu-list');
 const subMenu = document.querySelectorAll('.navbar-submenu__content');
-console.log(menuList);
 
 menuList.forEach((menu, i) => {
   menu.addEventListener('mouseenter', () => {
@@ -18,4 +17,34 @@ menuList.forEach((menu, i) => {
       });
     });
   });
+});
+
+// 메인슬라이드
+const mainSlide = [
+  {
+    id: 1,
+    img: 'https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    text: '',
+  },
+  {
+    id: 1,
+    img: 'https://images.unsplash.com/photo-1493770348161-369560ae357d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    text: '',
+  },
+  {
+    id: 1,
+    img: 'https://images.unsplash.com/photo-1505935428862-770b6f24f629?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1167&q=80',
+    text: '',
+  },
+  {
+    id: 1,
+    img: 'https://images.unsplash.com/photo-1494859802809-d069c3b71a8a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    text: '',
+  },
+];
+
+const mainSlideContainer = document.querySelector('.main-slide__container');
+
+mainSlide.forEach((slide, index) => {
+  const setSlide = `<div><img src=${slide.img}></div>`;
 });
